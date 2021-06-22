414fe1da8d590dae9f511f340e7edc744c74bc73
DaNaRim <rangar.danarim@gmail.com>
2021-06-22 15:30:14 +0300

    grom-l3

---
+++ gromcode/L3-data_types/scrypt.js
@@ -0,0 +1,11 @@
+const id = 1001;
+let price = 5000;
+let name = 'Iphone';
+const color = 'Silver';
+let isAvailable = true;
+let storeAddress = null;
+
+price = '6500'
+console.log(price);
+
+let weight = undefined;
